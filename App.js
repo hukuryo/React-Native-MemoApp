bbfabf11f5ba7da8de84ccb8b7ed641941bd9950
hukuryo <ryohei.1998.05.09@gmail.com>
2023-10-16 22:46:09 +0900

    Detailスクリーンを追加

--- App.js
+++ App.js
@@ -2,23 +2,16 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "./screens/HomeScreen";
+import { DetailScreen } from "./screens/DetailScreen";
 
 const Stack = createNativeStackNavigator();
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
